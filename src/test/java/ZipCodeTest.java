78c8f5e9b1dc106e989a8516fe78f23f6cfae3c1
ALIAKSEI LOKHACH <alexis.belyakoff@yandex.ru>
2025-04-21 22:07:53 +0300

    Start write test

---
+++ src/test/java/ZipCodeTest.java
@@ -0,0 +1,14 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class ZipCodeTest {
+
+    @Test
+    public void checkZipCode4Digits() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.sharelane.com/cgi-bin/register.py");
+        driver.quit();
+    }
+}
