7eaa53bd4987e7d1ff8b7fb0ce551a7ca316e440
ALIAKSEI LOKHACH <alexis.belyakoff@yandex.ru>
2025-04-23 20:11:24 +0300

    Added ZipCodeTest

--- src/test/java/ZipCodeTest.java
+++ src/test/java/ZipCodeTest.java
@@ -1,6 +1,10 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
 
 public class ZipCodeTest {
 
@@ -8,7 +12,15 @@ public class ZipCodeTest {
     public void checkZipCode4Digits() {
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10L, TimeUnit.SECONDS);
         driver.get("https://www.sharelane.com/cgi-bin/register.py");
+
+        driver.findElement(By.name("zip_code")).sendKeys("1234");
+        driver.findElement(By.cssSelector("[value=Continue]")).click();
+
+        String actualErrorMsg = driver.findElement(By.className("error_message")).getText();
+        Assert.assertEquals(actualErrorMsg, "Oops, error on page. ZIP code should have 5 digits");
+
         driver.quit();
     }
 }
